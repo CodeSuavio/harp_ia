389ba4469858c437f38a71ed8bfeb3ac55b57259
Ana Perry <anabperry@hotmail.com>
2026-09-19 15:16:05 -0300

    validations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,6 @@ class Poll < ApplicationRecord
   belongs_to :bill
 
   has_many :votes
+
+  validates :date, :description, presence: true
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ApplicationRecord
   belongs_to :poll
   belongs_to :deputy
+
+  validates :vote, presence: true
+  validates :vote, inclusion: { in: %w[Sim Não Abstenção Obstrução] }
+  validates :deputy_id, uniqueness: { scope: :poll_id, message: "só pode registrar um voto por votação" }
 end
